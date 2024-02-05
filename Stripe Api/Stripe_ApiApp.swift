d903f63b88060002e14abf050e0145abb9ef428e
Jacquese Whitson <jacqwhitsonappledev@gmail.com>
2024-02-04 19:56:27 -0500

    Initial Commit

---
+++ Stripe Api/Stripe_ApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stripe_ApiApp.swift
+//  Stripe Api
+//
+//  Created by Jacquese Whitson on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Stripe_ApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
